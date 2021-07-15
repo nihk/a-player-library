d26a7ee6ac83c1776b9c6b26c769ab709cdd29fe
Nick Rose <nickrose@microsoft.com>
2021-07-15 09:50:21 -0700

    Remove unused file

--- sample/src/main/kotlin/nick/sample/data/utils.kt
+++
@@ -1,9 +0,0 @@
-package nick.sample.data
-
-import android.view.View
-import android.view.ViewGroup
-
-fun View.detachFromParent() {
-    val parent = parent as? ViewGroup ?: return
-    parent.removeView(this)
-}
