83790429912b19342220e99a740344a3ede310d2
Nick Rose <nickrose@microsoft.com>
2021-07-20 11:19:14 -0700

    Fix broken test

--- player-ui-default/src/androidTest/kotlin/player/ui/def/FakeNavigator.kt
+++ player-ui-default/src/androidTest/kotlin/player/ui/def/FakeNavigator.kt
@@ -3,7 +3,7 @@ package player.ui.def
 import player.common.TrackInfo
 
 class FakeNavigator : Navigator {
-    override fun toTracksPicker(type: TrackInfo.Type) {
+    override fun toTracksPicker(type: TrackInfo.Type, onDismissed: () -> Unit) {
         error("unused")
     }
 }
